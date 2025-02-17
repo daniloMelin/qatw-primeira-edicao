7a7feb0877b8bda006bfaf806a136e6104001636
Danilo Melin <dmelin88@gmail.com>
2025-02-16 21:00:23 -0300

    melhorado o timeout e pesquisa do código de autentificaçãi

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,26 +1,52 @@
 import { test, expect } from '@playwright/test';
 
+import { obterCodigo2FA } from '../support/db';
+
+import { LoginPage } from '../pages/LoginPage';
+
+import { DashPage } from '../pages/DashPage';
+import { TIMEOUT } from 'dns';
+
 test('O usuário não deve logar se o código de autenticação for inválido.', async ({
   page,
 }) => {
+  const loginPage = new LoginPage(page);
+
   const usuario = {
     cpf: '00000014141',
     senha: '147258',
   };
-  await page.goto('http://paybank-mf-auth:3000/');
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(usuario.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
+  await loginPage.acessaPagina();
+  await loginPage.informaCPF(usuario.cpf);
+  await loginPage.informaSenha(usuario.senha);
 
-  for (const digito of usuario.senha) {
-    await page.getByRole('button', { name: digito }).click();
-  }
-
-  await page.getByRole('button', { name: 'Continuar' }).click();
-
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
+  await loginPage.informa2FA('123456');
 
   await expect(page.locator('span')).toContainText(
     'Código inválido. Por favor, tente novamente.'
   );
+});
+
+test('Deve acessar a conta do usuário', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  const dashPage = new DashPage(page);
+
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258',
+  };
+
+  await loginPage.acessaPagina();
+  await loginPage.informaCPF(usuario.cpf);
+  await loginPage.informaSenha(usuario.senha);
+
+  // Garantindo que o código foi processado. checkpoint
+  await page
+    .getByRole('heading', { name: 'Verificação em duas etapas' })
+    .waitFor({ timeout: 3000 });
+
+  const codigo = await obterCodigo2FA(usuario.cpf);
+  await loginPage.informa2FA(codigo);
+
+  await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00');
 });
